b43892d394a18008bedcede76c64518ef8cfd997
BenjaminAJ <ajanakub30@gmail.com>
2023-06-26 12:57:31 +0100

    added log in feature

---
+++ js/login.js
@@ -0,0 +1,41 @@
+const loginForm= document.querySelector('#loginForm');
+
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    login();
+    // window,location.href = '../userpage.html';
+});
+
+function doesUserExist(){
+    let userName = loginForm.email.value;
+    let userList = localStorage.getItem('userlist');
+    userList = JSON.parse(userList);
+
+    userList = userList.filter((user) => {return user.email === userName})
+
+    if (userList.length > 0) {
+        console.log('User found');
+        return true;
+    }
+    console.error('User not found');
+    return false;
+}
+
+function login() {
+    let does = doesUserExist();
+    if (does) {
+        let inputEmail = loginForm.email.value;
+        let inputPassword = loginForm.password.value;
+        let userList = localStorage.getItem('userlist');
+        userList = JSON.parse(userList);    
+        let user = userList.filter((user) => {return user.email === inputEmail})
+        if (inputPassword === user[0].password) {
+            console.log('You are logged in');
+            window.location.href = '/userpage.html';
+            return            
+        }
+        console.error('Enter valid password');
+    }
+    console.error('Enter a valid email address');
+}
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const registrationForm = document.querySelector('#registrationForm');
 
 registrationForm.addEventListener('submit', (event) =>{
     event.preventDefault();
-    BankUser.addUser();
+    Bank.addUser();
 })
 
 
@@ -10,31 +10,12 @@ registrationForm.addEventListener('submit', (event) =>{
 class Bank{
     static userlist=[];
 
-    constructor(bankName){
+
+    constructor(bankName = 'GTBank'){
         this.bankName = bankName;
     }
-}
 
-//BankUser
-class BankUser extends Bank{
-    #password;
-    balance = 0;
-
-    constructor(name, email, password, balance){
-        super();
-       this.#password = password;
-       this.name = name;
-       this.email = email;
-       this.accountNumber = BankUser.createAccountNumber();
-       this.balance = balance;   
-     }
-
-     static dispayUsers(){
-        let userlist = localStorage.getItem('userlist');
-        userlist = JSON.parse(userlist);
-        console.log(userlist);
-     }
-     static addUser(){
+    static addUser(){
         let name = registrationForm.name.value;
         let email = registrationForm.email.value;
         const repeatPassword = registrationForm.repeatPassword.value;
@@ -50,9 +31,34 @@ class BankUser extends Bank{
         let userlist = BankUser.userlist;
         localStorage.setItem('userlist', JSON.stringify(userlist));
 
-        window.location.href = "./userpage.html";
+        window.location.href = "./login.html";
 
      }
+
+     static dispayUsers(){
+        let userlist = localStorage.getItem('userlist');
+        userlist = JSON.parse(userlist);
+        console.log(userlist);
+     }
+
+
+    
+}
+
+//BankUser
+class BankUser extends Bank{
+    // #password;
+
+    constructor(name, email, password, balance, bankName){
+        super();
+       this.password = password;
+       this.name = name;
+       this.email = email;
+       this.accountNumber = BankUser.createAccountNumber();
+       this.balance = 0;   
+       this.bankName = super.bankName;
+     }
+
      static createAccountNumber(){
         let acctNumber='';
         for (let index = 0; index < 10; index++) {
